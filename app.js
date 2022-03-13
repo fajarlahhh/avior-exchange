dd062ed90f346676300e19e46c3fa63ee8141a54
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-03-13 21:34:16 +0800

    app js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express(); 
 const path = require('path')
-
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+  app.use(requireHTTPS);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
