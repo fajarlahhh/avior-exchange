b118816480f65ff973ed57d84ba17ee1ca088c69
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-03-20 18:01:30 +0800

    port

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-require('dotenv').config()
+const express = require('express');
+const app = express(); 
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-
-
-const app = express();
 const port = process.env.APP_PORT
+app.use(express.static('public'));
 
-app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({ extended : true }))
-app.use(express.static('public'))
-app.use(expressLayouts)
-
-app.set('layout', './')
-
-const routes = require('./routes/app.js')
-app.use('/', routes)
-
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname}); 
 });
+
+app.listen(port);
