1c11fb1f641c194f2e3e92f8829c7ef20d3849c9
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-03-28 15:42:10 +0800

    fix

--- app.js
+++ app.js
@@ -6,7 +6,34 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 const mysql = require('mysql')
 const uuid = require('uuid')
-const cors = require('cors')
+const router = express.Router();
+const session = require('express-session')
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
+const app = express();
+
+dotenv.config()
+const redisClient = redis.createClient({
+    host: '127.0.0.1',
+    port: 6379,
+    legacyMode: true
+})
+redisClient.connect().catch(console.error)
+
+app.use(session({
+    store: new RedisStore({ client: redisClient }),
+    secret: '123$asdf%asda3s^a',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false, // if true only transmit cookie over https
+        httpOnly: false, // if true prevent client side JS from reading the cookie
+    }
+}))
+
+const portHttps = process.env.APP_PORT_HTTPS
+const portHttp = process.env.APP_PORT_HTTP
+const aviorPrice = process.env.AVIOR_PRICE
 
 var unique = null
 
@@ -27,7 +54,6 @@ let options = {
     key : fs.readFileSync('./certificate/aviortoken_com.key')
 };
 
-const app = express();
 const httpsServer = https.createServer(options, app);
 const httpServer = http.createServer(app);
 
@@ -37,23 +63,15 @@ app.use((req, res, next) => {
     }
     next();
 });
-dotenv.config()
-
-const portHttps = process.env.APP_PORT_HTTPS
-const portHttp = process.env.APP_PORT_HTTP
-const aviorPrice = process.env.AVIOR_PRICE
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/ip', async (req, res) => {
-    res.send(req.socket.remoteAddress)
-})
+app.use('/', router);
 
-app.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
     usdtPrice = 0
     await axios.get('https://api.pancakeswap.info/api/v2/tokens/0x55d398326f99059fF775485246999027B3197955').then(data => {
         usdtPrice = parseFloat(data.data.data.price).toFixed(5)
@@ -64,36 +82,39 @@ app.get('/', async (req, res) => {
     res.render('index', { usdtPrice: usdtPrice, aviorPrice: aviorPrice })
 });
 
-app.get('/form', async (req, res) => {
-    res.render('form')
+router.post('/login', async (req,res) => {
+    const sess = req.session;
+    const { account } = req.body
+    sess.account = account
+    await db.query('DELETE FROM transaction WHERE address = ? and txid_client is null', [ sess.account ]);
+    res.end("form")
 });
 
-app.post('/admin', async (req, res) => {
-    const {
-        account
-    } = req.body
-    if (account == '0x98cfb452e87a506C96Fd06D46d3143eAe15110D0') {
-        await db.query('SELECT * FROM transaction where status = 0 and txid_client is not null', (error, elements) => {
-            if (error) {
-                res.render('admin', { result: [] })
-            }
-            res.render('admin', { result: elements})
-        });
-    }
-    res.send('No access')
-});
-
-app.post('/delete', async (req, res) => {
-    const {
-        account
-    } = req.body
-    await db.query('DELETE FROM transaction WHERE address = ? and txid_client is null', [account], (error, elements) => {
-        if (error) {
-            console.log(error)
-            return res.send(null);
+router.post('/logout',(req,res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
         }
-        return res.send(account)
     });
+    res.end("success")
+});
+
+router.get('/form', async (req, res) => {
+    if(req.session.account){
+        const account = req.session.account
+        if (account === '0x98cfb452e87a506C96Fd06D46d3143eAe15110D0') {
+            await db.query('SELECT * FROM transaction where status = 0 and txid_client is not null limit 20', (error, elements) => {
+                if (error) {
+                    res.render('admin', { result: [] })
+                }
+                res.render('admin', { result: elements})
+            });
+        }else{
+            res.render('client', { account: "Hy, " + account.substr(0,6) + "...." + account.substr(account.length - 10) })
+        }
+    }else{
+        res.send('No access')
+    }
 });
 
 app.post('/create', async (req,res) =>{
@@ -104,18 +125,22 @@ app.post('/create', async (req,res) =>{
     } = req.body
     unique = uuid.v1()
 
-    if (unique) {
-        await db.query('INSERT INTO transaction(`unique`, address, usdt_amount, avior_amount, status, created_at, updated_at) VALUES (?, ?, ?, ?, 0, now(), now())', [
-            unique,
-            account,
-            usdt,
-            avior
-        ], (error, elements) => {
-            if (error) {
-                return res.send(null);
-            }
-            return res.send(unique)
-        });
+    if(req.session.account){
+        if (unique) {
+            await db.query('INSERT INTO transaction(`unique`, address, usdt_amount, avior_amount, status, created_at, updated_at) VALUES (?, ?, ?, ?, 0, now(), now())', [
+                unique,
+                account,
+                usdt,
+                avior
+            ], (error, elements) => {
+                if (error) {
+                    return res.send(null);
+                }
+                return res.send(unique)
+            });
+        }
+    } else {
+        return res.status(403).send(null);
     }
 });
 
@@ -125,35 +150,45 @@ app.post('/update', async (req,res) =>{
         txid
     } = req.body
 
-    if (unique) {
-        await db.query('UPDATE transaction SET txid_client=? WHERE `unique`=?', [
-            txid,
-            unique
-        ], (error, elements) => {
-            if (error) {
-                return res.send(null);
-            }
-            return res.send(unique)
-        });
+    if(req.session.account){
+        if (unique) {
+            await db.query('UPDATE transaction SET txid_client=? WHERE `unique`=?', [
+                txid,
+                unique
+            ], (error, elements) => {
+                if (error) {
+                    return res.send(null);
+                }
+                return res.send(unique)
+            });
+        }
+    } else {
+        return res.status(403).send(null);
     }
 });
 
-app.post('/sukses', async (req,res) =>{
+app.post('/send', async (req,res) =>{
     const {
         unique,
         txid
     } = req.body
 
-    if (unique) {
-        await db.query('UPDATE transaction SET txid_admin=?,status=1 WHERE `unique`=?', [
-            txid,
-            unique
-        ], (error, elements) => {
-            if (error) {
-                return res.send(null);
+    if(req.session.account){
+        if (req.session.account) {
+            if (unique) {
+                await db.query('UPDATE transaction SET txid_admin=?,status=1 WHERE `unique`=?', [
+                    txid,
+                    unique
+                ], (error, elements) => {
+                    if (error) {
+                        return res.send(null);
+                    }
+                    return res.send(unique)
+                });
             }
-            return res.send(unique)
-        });
+        }
+    } else {
+        return res.status(403).send(null);
     }
 });
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-var account, web3, usdtNeed, aviorNeed
+var account, web3, usdtNeed
 
 const provider = new WalletConnectProvider.default({
     rpc: {
@@ -19,313 +19,6 @@ window.addEventListener('DOMContentLoaded', (e) => {
     lblAviorUsdtPrice.innerText = parseFloat(aviorPrice/usdtPrice).toFixed(5) + " USDT"
 })
 
-const sendTransaction = async () => {
-    try {
-        $('#section-form').hide()
-        $('#section-waiting').show() 
-
-        var contractAbi = [
-            {
-                "anonymous": false,
-                "inputs": [
-                    {
-                        "indexed": true,
-                        "internalType": "address",
-                        "name": "owner",
-                        "type": "address"
-                    },
-                    {
-                        "indexed": true,
-                        "internalType": "address",
-                        "name": "spender",
-                        "type": "address"
-                    },
-                    {
-                        "indexed": false,
-                        "internalType": "uint256",
-                        "name": "value",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "Approval",
-                "type": "event"
-            },
-            {
-                "anonymous": false,
-                "inputs": [
-                    {
-                        "indexed": true,
-                        "internalType": "address",
-                        "name": "from",
-                        "type": "address"
-                    },
-                    {
-                        "indexed": true,
-                        "internalType": "address",
-                        "name": "to",
-                        "type": "address"
-                    },
-                    {
-                        "indexed": false,
-                        "internalType": "uint256",
-                        "name": "value",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "Transfer",
-                "type": "event"
-            },
-            {
-                "constant": true,
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "_owner",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "spender",
-                        "type": "address"
-                    }
-                ],
-                "name": "allowance",
-                "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": false,
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "spender",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amount",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "approve",
-                "outputs": [
-                    {
-                        "internalType": "bool",
-                        "name": "",
-                        "type": "bool"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "nonpayable",
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "account",
-                        "type": "address"
-                    }
-                ],
-                "name": "balanceOf",
-                "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [],
-                "name": "decimals",
-                "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [],
-                "name": "getOwner",
-                "outputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [],
-                "name": "name",
-                "outputs": [
-                    {
-                        "internalType": "string",
-                        "name": "",
-                        "type": "string"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [],
-                "name": "symbol",
-                "outputs": [
-                    {
-                        "internalType": "string",
-                        "name": "",
-                        "type": "string"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [],
-                "name": "totalSupply",
-                "outputs": [
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "constant": false,
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "recipient",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amount",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "transfer",
-                "outputs": [
-                    {
-                        "internalType": "bool",
-                        "name": "",
-                        "type": "bool"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "nonpayable",
-                "type": "function"
-            },
-            {
-                "constant": false,
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "sender",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "recipient",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "amount",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "transferFrom",
-                "outputs": [
-                    {
-                        "internalType": "bool",
-                        "name": "",
-                        "type": "bool"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "nonpayable",
-                "type": "function"
-            }
-        ]
-        var tokenAddress = '0x55d398326f99059fF775485246999027B3197955'
-        var fromAddress = account
-        window.onbeforeunload = function() {
-            return "Dude, are you sure you want to leave? Think of the kittens!";
-        }
-        var contract = new web3.eth.Contract(contractAbi, tokenAddress)
-        var unique = null;
-        await $.post("/create", {
-            account : account,
-            usdt : usdtNeed,
-            avior : aviorNeed
-        }, function(data, status){
-            if (status == 'success') {
-                unique = data
-            }else{
-                alert('Transaction Failed')
-    
-                window.onbeforeunload = null
-                view.classList.remove('hidden')
-                return
-            }
-        });
-        
-        await contract.methods.transfer('0x98cfb452e87a506C96Fd06D46d3143eAe15110D0', web3.utils.toWei(usdtNeed.toString(), 'ether')).send({
-            from: fromAddress, gas: 100000},function (error, result){ 
-            if(!error){
-                $.post("/update", {
-                    unique : unique,
-                    txid : result
-                }, function(data, status){
-                    if (!status == 'success') {
-                        alert('Transaction Failed')
-                    }else{
-                        alert('Your transaction was successful. Your avior will be sent to your wallet within 1x24 hours')
-                    }
-                });
-            }
-        });
-    } catch (error) {
-        alert(error.message)
-    }
-    window.onbeforeunload = null
-    location.reload()
-}
-
 const connect = async () => {
     $("#section-form").show()
     try {
@@ -338,23 +31,11 @@ const connect = async () => {
         var accounts = await web3.eth.getAccounts()
         account = accounts[0]
 
-        await $.post("/delete", {
-            account : account,
-        }, function(data, status){
-            if (status != 'success') {
-                location.reload()
-                return
-            }
+        $.post("/login",{  account:account },function(data){
+            $("#section-connect").hide()
+            btnDisconnect.classList.remove('hidden')
+            $("#form").load("/" + data);
         });
-
-        if (account == '0x98cfb452e87a506C96Fd06D46d3143eAe15110D0') {
-            $("#form").load("/admin");
-        } else {
-            $("#form").load("/form");
-        }
-
-        $("#section-connect").hide()
-        btnDisconnect.classList.remove('hidden')
     } catch (error) {
         alert(error.message)
         location.reload()
@@ -364,12 +45,15 @@ const connect = async () => {
 btnConnect.onclick = connect
 
 btnDisconnect.onclick = async () => {
-    await provider.disconnect()
-    .then(() => console.log("Provider disconnected"))
-    .catch(() => location.reload());
-
-    $("#section-connect").show()
-    $("#section-form").hide()
-
-    btnDisconnect.classList.add('hidden')
+    await $.post("/logout", [],function(data){
+        if (data === 'success') {
+            provider.disconnect()
+            .then(() => {
+                btnDisconnect.classList.add('hidden')
+                $("#section-connect").show()
+                $("#section-form").hide()
+            })
+            .catch(() => location.reload());
+        }
+    })
 } 
